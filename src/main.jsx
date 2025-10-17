318fc9856b94850ebc4ab9c2103f299dc63c6cbb
Ingetes <comercial@ingetes.com>
2025-10-17 07:42:02 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import PortalDistribuidoresLanding from './PortalCanales.jsx'
+import PortadaPortalClientes from './PortadaPortalClientes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PortalDistribuidoresLanding />
+    <PortadaPortalClientes />
   </React.StrictMode>
 )
