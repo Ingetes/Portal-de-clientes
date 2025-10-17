198c2410745f1594874478ad62ac7ad121897354
Ingetes <comercial@ingetes.com>
2025-10-17 07:34:02 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import PortalDistribuidoresLanding from './PortalCanales.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortalDistribuidoresLanding />
+  </React.StrictMode>
+)
