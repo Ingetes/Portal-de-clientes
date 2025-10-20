74c60ee80caa4e51850cabdec1c0e8384d582eea
Ingetes <comercial@ingetes.com>
2025-10-20 08:26:31 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import PortalAccesoClientes from './portal_de_acceso_clientes.jsx' // 👈 nombre y ruta correctos
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PortalAccesoClientes />
-  </React.StrictMode>
-)
+// Vistas
+import PortadaPortalClientes from "./PortadaPortalClientes.jsx";
+import PortalClientesAuth from "./portal_de_acceso_clientes.jsx";
+import PortalAdminUsuarios from "./portal_administrador_de_usuarios.jsx";
+import IngetesAdmin from "./ingetes_admin.jsx";
+
+const routes = {
+  "": PortadaPortalClientes,        // sin hash
+  "#home": PortadaPortalClientes,   // home
+  "#ingresar": PortalClientesAuth,  // login
+  "#admin": PortalAdminUsuarios,    // admin de usuarios
+  "#ingetes-admin": IngetesAdmin,   // backoffice demo
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function mount() {
+  const hash = window.location.hash || "#home";
+  const View = routes[hash] || PortadaPortalClientes;
+  root.render(<View />);
+}
+
+window.addEventListener("hashchange", mount);
+mount(); // primer render
